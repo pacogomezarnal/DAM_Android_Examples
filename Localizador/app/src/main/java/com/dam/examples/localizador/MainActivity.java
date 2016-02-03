8915084cd4663b1d1f4ed8f8429b69bbb3c3512f
Paco Gomez <fjgomez@florida-uni.es>
2016-02-04 00:30:09 +0100

    Localizador con Google maps

--- Localizador/app/src/main/java/com/dam/examples/localizador/MainActivity.java
+++ Localizador/app/src/main/java/com/dam/examples/localizador/MainActivity.java
@@ -8,18 +8,26 @@ import android.util.Log;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends AppCompatActivity implements
         GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener  {
-    //
+        GoogleApiClient.OnConnectionFailedListener, OnMapReadyCallback {
+
     private GoogleApiClient mGoogleApiClient;
     private Location mLastLocation;
     protected static final String TAG="Localizando";
+    private GoogleMap mMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setUpMapIfNeeded();
 
         // Create an instance of GoogleAPIClient.
         if (mGoogleApiClient == null) {
@@ -30,7 +38,15 @@ public class MainActivity extends AppCompatActivity implements
                     .build();
         }
     }
-
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+        }
+    }
     @Override
     public void onConnected(Bundle bundle) {
 
@@ -41,6 +57,15 @@ public class MainActivity extends AppCompatActivity implements
             if (mLastLocation != null) {
                 Log.i(TAG, String.valueOf(mLastLocation.getLatitude()));
                 Log.i(TAG, String.valueOf(mLastLocation.getLongitude()));
+                double currentLatitude = mLastLocation.getLatitude();
+                double currentLongitude = mLastLocation.getLongitude();
+                LatLng latLng = new LatLng(currentLatitude, currentLongitude);
+
+                MarkerOptions options = new MarkerOptions()
+                        .position(latLng)
+                        .title("Aquí estoy!");
+                mMap.addMarker(options);
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
             }
         }catch (SecurityException e){
             Log.i(TAG,"Denegada la localización");
@@ -66,5 +91,10 @@ public class MainActivity extends AppCompatActivity implements
         if(mGoogleApiClient.isConnected())
             mGoogleApiClient.disconnect();
         super.onStop();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
     }
 }
